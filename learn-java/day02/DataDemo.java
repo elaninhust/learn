a3df037e3b74558d9da049f9185969bfd1d3ef89
elan <tanfan@idstaff.com>
2017-07-13 15:42:51 +0800

    update day02/README.md

---
+++ learn-java/day02/DataDemo.java
@@ -0,0 +1,31 @@
+/*
+	java基本数据类型的demo
+	4类8种
+ */
+class DataDemo{
+	public static void main(String[] args){
+		byte i1 = 1;
+		System.out.println(i1);
+
+		int i2 = 2;
+		System.out.println(i2);
+
+		short i3 = 3;
+		System.out.println(i3);
+
+		long i4 = 124L;
+		System.out.println(i4);
+
+		float j1 = 0.234F;
+		System.out.println(j1);
+
+		double j2 = 0.35435;
+		System.out.println(j2);
+
+		boolean k1 = true;
+		System.out.println(k1);
+
+		char l1 = 'a';
+		System.out.println(l1);
+	}
+}
